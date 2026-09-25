cb031fd5fa5ce12e85246dbc849da9bbe210c925
Daria <dariasoliarova@gmail.com>
2026-09-25 02:28:10 +0200

    .

--- lab2.2.py
+++
@@ -1,9 +0,0 @@
-# Імпортуємо модуль
-import task2
-
-def main():
-    # Запускаємо функцію
-    task2.run_task2()
-
-if __name__ == "__main__":
-    main()
---
+++ main.py
@@ -0,0 +1,9 @@
+# ²ìïîğòóºìî ìîäóëü
+import task2
+
+def main():
+    # Çàïóñêàºìî ôóíêö³ş
+    task2.run_task2()
+
+if __name__ == "__main__":
+    main()
